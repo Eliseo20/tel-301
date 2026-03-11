0b012fee5a42ea7927398ab5092f053a85af2cdb
Eliseo <ecornejoa@docente.cl>
2026-03-11 06:30:19 -0300

    Trigger deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// GitHub Pages Deployment Trigger
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/tel-301/',
